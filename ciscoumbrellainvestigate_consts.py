7603e77c45e96c7574077ceba8ccc16b8e436afa
Ronald Gil <rgil@splunk.com>
2022-02-02 17:42:15 -0800

    Fix semgrep warnings

--- ciscoumbrellainvestigate_consts.py
+++ ciscoumbrellainvestigate_consts.py
@@ -12,6 +12,8 @@
 # the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
 # either express or implied. See the License for the specific language governing permissions
 # and limitations under the License.
+DEFAULT_TIMEOUT = 30
+
 INVESTIGATE_JSON_DOMAIN = "domain"
 INVESTIGATE_JSON_IP = "ip"
 INVESTIGATE_JSON_APITOKEN = "access_token"
